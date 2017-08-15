27256fbc2639e99f8a4f2947757f9c6f84a156ae
Christie Beukman <me.mcbeukman@gmail.com>
2017-08-15 19:47:23 +0200

    initial commit

---
+++ Nippon Load.bat
@@ -0,0 +1,8 @@
+@echo off
+start %windir%\explorer.exe D:\Documents\Projects\SAAB\Nippon\Technical Manual\ISSUE 01\Nippon Repo\"
+start %windir%\explorer.exe D:\Documents\Projects\SAAB\Nippon\Technical Manual\DRAFTS
+cd /d C:\Program Files (x86)\MediaMonkey\
+start MediaMonkey.exe
+start %windir%\explorer.exe D:\Documents\Projects\SAAB\Nippon\Source Material\Drawings
+cd /d C:\Program Files\Oracle\VirtualBox\
+start VirtualBox.exe
